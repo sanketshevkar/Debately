eb3be52b4e6b71a9436ec23334a7e8996ec763e0
Sanket Shevkar <shevkar.sanket@gmail.com>
2021-05-28 22:05:57 +0530

    bug(client): eslint removed
    Signed-off-by: Sanket Shevkar <shevkar.sanket@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import React, {useState} from "react"
+import React, { useState, useRef } from 'react';
 
 function App() {
-  const [state,setState] = useState('')
-  const onClick = (e) => {
-    setState('Clicked')
-    console.log(e)
-  }
+  const [mediaButton, setMediaButton] = useState(false);
+  const [createRoomButton, setCreateRoomButton] = useState(true);
+  const [joinRoomButton, setJoinRoomButton] = useState(true);
+  const [hangUpButton, setHangUpButton] = useState(true);
+  const [roomId, setRoomId] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  const localStream = useRef();
+  const remoteStream = useRef();
 
   return (
     <div className="App">
-    <button onClick={onClick}>Click me!</button>
-    <div>{state}</div>
+      <button type="button">create room</button>
+      <button type="button">join room</button>
+      <button type="button">hang up</button>
     </div>
   );
 }
