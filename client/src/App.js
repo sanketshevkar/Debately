0c00304fc2b673525c9c04bfd1d113fa2a38cb1a
Gatij Taranekar <gatij.taranekar@gmail.com>
2021-05-28 20:31:19 +0530

    feat(client): Add button and onclick function
    Signed-off-by: Gatij Taranekar <gatij.taranekar@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [state,setState] = useState('')
+  const onClick = (e) => {
+    setState('Clicked')
+    console.log(e)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={onClick}>Click me!</button>
+    <div>{state}</div>
     </div>
   );
 }
