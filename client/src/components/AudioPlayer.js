2557ce939a3ec13515aa06d3caeb12d441126fad
Sanket Shevkar <shevkar.sanket@gmail.com>
2021-05-29 10:42:45 +0530

    feat(client): debateroom skeleton
    Signed-off-by: Sanket Shevkar <shevkar.sanket@gmail.com>

---
+++ client/src/components/AudioPlayer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AudioPlayer = () => {
+
+  return (
+    <div>
+      <div>
+          Avatar
+      </div>
+      <div>
+          username
+      </div>
+      <div>
+          status
+      </div>
+      <button>
+          mute
+      </button>
+    </div>
+  );
+};
+export default AudioPlayer;
