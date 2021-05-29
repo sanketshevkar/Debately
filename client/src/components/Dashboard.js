a5316f7eb2a2f55ac2883e1d98b76b6227ace627
Sanket Shevkar <shevkar.sanket@gmail.com>
2021-05-29 21:25:18 +0530

    chore(client): design change
    Signed-off-by: Sanket Shevkar <shevkar.sanket@gmail.com>

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,28 +1,52 @@
-import React, { useContext } from 'react';
-import { useHistory, withRouter } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import { UserContext } from '../context/UserContext';
-import { logoutUser } from '../services/magic';
+import React, { useContext, useState } from "react";
+import { useHistory, withRouter } from "react-router-dom";
+import { Button } from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+import ProfilePage from "./ProfilePage";
+import ProfileViewer from "./ProfileViewer";
+import { UserContext } from "../context/UserContext";
+import { logoutUser } from "../services/magic";
+import DebateRoom from "./DebateRoom";
 const Dashboard = () => {
   const { email } = useContext(UserContext);
   const history = useHistory();
   const handleLogOut = async () => {
     try {
       await logoutUser();
-      history.replace('/');
+      history.replace("/");
     } catch (error) {
       console.error(error);
     }
   };
+  if(debate === true){
+    return(
+      <div style={{ margin: "2rem" }}>
+          <DebateRoom />
+        </div>
+    )
+  }else{
+    return( <div style={{ margin: "2rem" }}>
+    <ProfileViewer/>
+  </div>
+  )
+
+  }
   return (
-    <div className="p-2">
-      <div className="d-flex justify-content-end">
-        <Button variant="primary" onClick={handleLogOut}>
-          Sign Out
-        </Button>
+    <div>
+        <div style={{display: "flex",
+          justifyContent: "center",
+            alignItems: "center", marginTop: '2rem'}}>
+          <Button
+            type="danger"
+            shape="round"
+            icon={<LogoutOutlined />}
+            size="large"
+            onClick={handleLogOut}
+          >
+            Sign Out
+          </Button>
+        </div>
       </div>
-      <h1 className="h1">User: {email}</h1>
-    </div>
   );
 };
 export default withRouter(Dashboard);
