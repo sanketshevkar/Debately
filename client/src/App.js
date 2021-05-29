78b1727e3d5bb5fe65768a0354aba28704e5e46e
Sanket Shevkar <shevkar.sanket@gmail.com>
2021-05-29 14:34:11 +0530

    feat(client): debateroom webrtc integration
    Signed-off-by: Sanket Shevkar <shevkar.sanket@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import './App.css';
-// import {
-//   Switch,
-//   BrowserRouter as Router,
-//   Route,
-//   Redirect,
-// } from "react-router-dom";
-// import Navbar from "../src/components/Navbar";
-// import Spinner from "react-bootstrap/Spinner";
-// import { UserContext } from "./context/UserContext";
-// import { checkUser } from "./services/magic";
-// import Authenticate from "./components/Authenticate";
-// import Dashboard from "./components/Dashboard";
-// import Meeting from "./components/Meeting";
-// import DebateRoom from "./components/DebateRoom";
-// import PrivateRoute from "./components/PrivateRoute";
-// const App = () => {
-//   const [user, setUser] = useState({ isLoggedIn: null, email: "" });
-//   const [loading, setLoading] = useState();
-//   useEffect(() => {
-//     const validateUser = async () => {
-//       setLoading(true);
-//       try {
-//         await checkUser(setUser);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     validateUser();
-//   }, [user.isLoggedIn]);
-//   if (loading) {
-//     return (
-//       <div
-//         // className="d-flex justify-content-center align-items-center"
-//         // style={{ height: "100vh" }}
-//       >
-//         <Spinner animation="border" />
-//       </div>
-//     );
-//   }
-//   return (
-//     <div>
-//       <Navbar />
-//       <UserContext.Provider value={user}>
-//         <Router>
-//           {user.isLoggedIn && <Redirect to={{ pathname: "/dashboard" }} />}
-//           <Switch>
-//             <Route exact path="/" component={Authenticate} />
-//             <PrivateRoute path="/dashboard" component={Dashboard} />
-//             <PrivateRoute path="/meetings" component={Meeting} />
-//             <PrivateRoute path="/debateRoom" component={DebateRoom} />
-//           </Switch>
-//         </Router>
-//       </UserContext.Provider>
-//     </div>
-//   );
-// };
-// export default App;
-
 import React, { useState, useEffect } from "react";
 import './App.css';
 import {
@@ -67,13 +6,54 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+import Spinner from "react-bootstrap/Spinner";
+import { UserContext } from "./context/UserContext";
+import { checkUser } from "./services/magic";
+import Authenticate from "./components/Authenticate";
+import Dashboard from "./components/Dashboard";
+import Meeting from "./components/Meeting";
 import DebateRoom from "./components/DebateRoom";
+import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
+  const [user, setUser] = useState({ isLoggedIn: null, email: "" });
+  const [loading, setLoading] = useState();
+  useEffect(() => {
+    const validateUser = async () => {
+      setLoading(true);
+      try {
+        await checkUser(setUser);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    validateUser();
+  }, [user.isLoggedIn]);
+  if (loading) {
+    return (
+      <div
+        // className="d-flex justify-content-center align-items-center"
+        // style={{ height: "100vh" }}
+      >
+        <Spinner animation="border" />
+      </div>
+    );
+  }
   return (
     <div>
+      <Navbar />
+      <UserContext.Provider value={user}>
         <Router>
-            <Route path="/" component={DebateRoom} />
+          {user.isLoggedIn && <Redirect to={{ pathname: "/dashboard" }} />}
+          <Switch>
+            <Route exact path="/" component={Authenticate} />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
+            <PrivateRoute path="/meetings" component={Meeting} />
+            <PrivateRoute path="/debateRoom" component={DebateRoom} />
+          </Switch>
         </Router>
+      </UserContext.Provider>
     </div>
   );
 };
